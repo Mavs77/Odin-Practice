03b95c9e42a4882e6d17e10c76033760a7c22e72
Laurent Tshisekedi <Laurent.N.Tshisekedi@gmail.com>
2024-10-06 21:11:22 -0500

    completed the digital library

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
-//Write a function that loops through the array and displays each book on the page. You can display them in some sort of table, or each on their own “card”. It might help for now to manually add a few books to your array so you can see the display.
-
 const button = document.getElementById('addBook'); 
 
 //makes the form visible when 'addBook' button is clicked so that the user can add their desired book. Also hides the shelf so they both don't take up too much space. 
